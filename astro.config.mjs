d29aaf8565ba5c285d855f092750e3ad3e0b4f9b
ivoinkwell <1552965545@qq.com>
2026-08-19 23:38:01 +0800

    更新域名显示

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://ivoinkwell.xyz/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
